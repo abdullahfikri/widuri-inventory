0deae8f6c0675670c16aea660a67d6e6a05ba300
muhammad fikri <mfikri.dev@gmail.com>
2024-06-30 23:33:41 +0700

    Adding Swagger UI for api doc display

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,9 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { WINSTON_MODULE_NEST_PROVIDER } from 'nest-winston';
+import * as fs from 'fs';
+import { join } from 'path';
+import * as swaggerUi from 'swagger-ui-express';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -8,6 +11,16 @@ async function bootstrap() {
   const logger = app.get(WINSTON_MODULE_NEST_PROVIDER);
   app.useLogger(logger);
   app.enableCors();
+
+  // Load your OpenAPI YAML file
+  const document = fs.readFileSync(
+    join(__dirname, '../docs/inventory.json'),
+    'utf8',
+  );
+
+  // Serve Swagger UI
+  app.use('/api', swaggerUi.serve, swaggerUi.setup(JSON.parse(document)));
+
   await app.listen(3000);
 }
 bootstrap();
